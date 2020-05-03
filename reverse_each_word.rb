a78bbc79e98d195f5443dcc4cdcd13632bae9ee9
Iona <ionabrabender@gmail.com>
2020-05-03 23:22:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,7 @@ require 'pry'
 def reverse_each_word(sentence)
   new_sentence = []
   new_sentence << sentence.split.map {|word| word.reverse}
-  new_sentence.join(" ")
-  binding.pry
+  new_sentence.join!(" ")
   new_sentence
 end
 
