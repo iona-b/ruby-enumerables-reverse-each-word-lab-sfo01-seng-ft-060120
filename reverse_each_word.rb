6a9100f5e030bb349f1b33d12a3f861fc977165a
Iona <ionabrabender@gmail.com>
2020-05-03 23:13:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def reverse_each_word(sentence)
   new_sentence = []
-  split_sentence = sentence.split
-  split_sentence.each do |word|
+  sentence.split!
+  sentence.each do |word|
     sentence.push(word.reverse)
   end
   new_sentence
