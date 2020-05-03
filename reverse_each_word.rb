21a0572a38b973bfbd0a44366452f669211d9e71
Iona <ionabrabender@gmail.com>
2020-05-03 23:05:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word(sentence)
   new_sentence = []
-  new_sentence << sentence.split.each {|word| word.reverse}.join(" ")
+  new_sentence.push(sentence.split.each {|word| word.reverse}.join(" "))
   new_sentence
 end
